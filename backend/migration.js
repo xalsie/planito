e1b6255b5d83a005670bcfbb0843905d23e5d53c
LeGrizzly <xalsie.ff@hotmail.fr>
2024-12-10 18:46:29 +0100

    feat(db): Add Event model & relationship with User feat(db): migrate event

--- backend/migration.js
+++ backend/migration.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const sequelize = require('./models/db');
 
 const User = require('./models/user');
+const Event = require('./models/event.models');
 
+User.hasMany(Event, { as: 'user_id' });
 
 sequelize.sync({ alter: true });
--- backend/models/db.js
+++ backend/models/db.js
@@ -16,9 +16,6 @@ const sequelize = new Sequelize(
     console.log("Connexion à la base de données établie avec succès.");
   } catch (error) {
     console.error("Impossible de se connecter à la base de données :", error);
-  } finally {
-    await sequelize.close();
-    console.log("Connexion à la base de données fermée.");
   }
 })();
 
