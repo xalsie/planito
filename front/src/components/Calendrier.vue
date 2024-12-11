15df259fd623fc6d42f71ec26c811a9781727de5
Erkant YILDIZ <52324424+EEEEEEKRAN@users.noreply.github.com>
2024-12-11 20:52:05 +0100

    Integration de full calendar

---
+++ front/src/components/Calendrier.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="bg-white p-4 rounded-lg shadow">
+    <FullCalendar 
+      :options="calendarOptions"
+    />
+  </div>
+</template>
+
+<script setup>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import frLocale from '@fullcalendar/core/locales/fr'
+import '../assets/styles/calendar.css'
+
+const calendarOptions = {
+  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+  initialView: 'timeGridWeek',
+  locale: frLocale,
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+  },
+  editable: true,
+  selectable: true,
+  selectMirror: true,
+  dayMaxEvents: true,
+  weekends: true,
+  events: [],
+  height: 'auto',
+  views: {
+    timeGrid: {
+      dayMaxEventRows: 4,
+      slotDuration: '01:00:00',
+      slotLabelFormat: {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      }
+    }
+  }
+}
+</script> 
