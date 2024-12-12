60fc8dd1a164cdb8d94740bd0e03a3cccd0eb1b1
Bass <bass@MacBook-Air-de-Bass.local>
2024-12-12 01:23:33 +0100

    updated

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,25 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import DashboardIntervenantView from '../views/DashboardIntervenantView.vue'
-import DashboardSchoolView from '../views/DashboardSchoolView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import DashboardIntervenantView from "../views/DashboardIntervenantView.vue";
+import DashboardSchoolView from "../views/DashboardSchoolView.vue";
+import IntervenantsListView from "../views/IntervenantsListView.vue";
+import RoomsListView from "../views/RoomsListView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      redirect: "/dashboard/intervenant",
+    },
+    {
+      path: "/dashboard/intervenant",
+      name: "dashboard-intervenant",
+      component: DashboardIntervenantView,
+    },
+    {
+      path: "/dashboard/ecole",
+      name: "dashboard-ecole",
+      component: DashboardSchoolView,
+      children: [
         {
-            path: '/',
-            redirect: '/dashboard/intervenant'
+          path: "intervenants",
+          name: "intervenants-list",
+          component: IntervenantsListView,
         },
         {
-            path: '/dashboard/intervenant',
-            name: 'dashboard-intervenant',
-            component: DashboardIntervenantView
+          path: "salles",
+          name: "rooms-list",
+          component: RoomsListView,
         },
-        {
-            path: '/dashboard/ecole',
-            name: 'dashboard-ecole',
-            component: DashboardSchoolView
-        }
-    ]
-})
+      ],
+    },
+  ],
+});
 
-export default router 
+export default router;
---
+++ front/src/views/IntervenantsListView.vue
@@ -0,0 +1,42 @@
+<template>
+    <Table title="Liste des intervenants" :columns="columns" :rows="rows" />
+</template>
+
+<script setup>
+import Table from '../components/Table.vue';
+const columns = [
+    { label: 'Nom', key: 'lastname' },
+    { label: 'Prénom', key: 'firstname' },
+    { label: 'E-mail', key: 'email' },
+    { label: 'Roles', key: 'roles' },
+    { label: 'Classe(s)', key: 'classes' },
+    { label: 'Module(s)', key: 'modules' },
+];
+
+
+const rows = [
+    {
+        lastname: 'DIKIADI',
+        firstname: 'Bass',
+        email: 'binda@bass.fr',
+        roles: 'ROLE_INTERVENANT',
+        classes: '5AIW, 4AIW, 3AIW',
+        modules: 'PHP, React, Symfony',
+    },
+    {
+        lastname: 'DIKIADI',
+        firstname: 'Bass',
+        email: 'binda@bass.fr',
+        roles: 'ROLE_INTERVENANT',
+        classes: '5AIW, 4AIW, 3AIW',
+        modules: 'PHP, React, Symfony',
+    }, {
+        lastname: 'DIKIADI',
+        firstname: 'Bass',
+        email: 'binda@bass.fr',
+        roles: 'ROLE_INTERVENANT',
+        classes: '5AIW, 4AIW, 3AIW',
+        modules: 'PHP, React, Symfony',
+    },
+];
+</script>
