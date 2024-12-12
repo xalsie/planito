a558033f09ff04a47323185200c5df46204f28ea
Taslima <taslimaahamedmze@gmail.com>
2024-12-12 16:59:07 +0100

    All events (courses and exam) for school

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/", eventController.find);
 router.post("/", eventController.create);
+router.get("/school/:schoolId", eventController.findCoursesBySchool);
 router.get("/:eventId", eventController.findById);
 router.patch("/:eventId", eventController.updateById);
 router.delete("/:eventId", eventController.deleteById);
